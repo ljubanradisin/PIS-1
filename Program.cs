ffe4987d3ddbeb0a198e923b59a4a5e76084c20f
Markobriga <78802411+Markobriga@users.noreply.github.com>
2021-03-29 10:55:25 +0200

    Ispravak bug-a

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Ovo je nova linija koda!");//promjena
+            Console.WriteLine("Ovo je nova linija koda");//promjena
 
         }
     }
